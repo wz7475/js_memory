20a539baf2378ee438f3fda32cb945a9b19a341a
Wojciech Zarzecki <wojtek.zarzecki2002@gmail.com>
2021-01-09 19:36:44 +0100

    a

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const generade_cards = () =>{
     const main = document.querySelector("main");
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 16; i++) {
         let card = document.createElement("div");
+        let img = document.createElement("img");
+        img.src = "img/pawn.svg";
         card.id = i;
         card.className = "card";
+        card.appendChild(img);
         main.appendChild(card);
     } 
 }   
